47eaabe7470bdb8a8162633f488d1fc8111ca9d9
ritonun <jeremy.moreau2001@gmail.com>
2024-09-20 16:04:54 +0200

    show fps

--- emulator/__main__.py
+++ emulator/__main__.py
@@ -2,9 +2,10 @@ import sys
 
 import pygame
 
-display = pygame.display.set_mode((64*4, 32*4))
+display = pygame.display.set_mode((64*8, 32*8))
 screen = pygame.Surface((64, 32))
-pygame.display.set_caption("Riton's PyChip8 Emulator")
+CAPTION = "Riton's PyChip8 Emulator"
+pygame.display.set_caption(CAPTION)
 clock = pygame.time.Clock()
 
 run = True
@@ -12,10 +13,14 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
     display.fill((0, 0, 0))
 
+    pygame.display.set_caption(f"{CAPTION} - {round(clock.get_fps(), 1)} fps")
+
     pygame.display.update()
-    clock.tick(60)
+    dt = clock.tick(60)
+    
 
 pygame.quit()
 sys.exit(0)
